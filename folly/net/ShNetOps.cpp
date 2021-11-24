d80ed425648fedec451e5fa3543845ea5fbd86db
Saubhik Mukherjee <saubhik.mukherjee@gmail.com>
2021-11-23 20:12:07 -0500

    Fix some issues for running mvfst

--- folly/net/ShNetOps.cpp
+++ folly/net/ShNetOps.cpp
@@ -4,25 +4,25 @@
 
 namespace folly::shnetops {
 
-int bind(ShNetworkSocket &s, const netaddr *name) {
+int bind(ShNetworkSocket& s, const netaddr* name) {
   if (s != ShNetworkSocket()) {
     close(s);
   }
 
   s.localAddr = *name;
-  rt::UdpConn *sock = rt::UdpConn::Listen(s.localAddr);
+  rt::UdpConn* sock = rt::UdpConn::Listen(s.localAddr);
   sock->SetNonblocking(s.nonBlocking);
   s.data = sock;
 
   return 0;
 }
 
-int close(ShNetworkSocket &s) {
+int close(ShNetworkSocket& s) {
   delete s.data;
   return 0;
 }
 
-int connect(ShNetworkSocket &s, const netaddr *name) {
+int connect(ShNetworkSocket& s, const netaddr* name) {
   if (s != ShNetworkSocket()) {
     close(s);
   }
@@ -34,31 +34,31 @@ int connect(ShNetworkSocket &s, const netaddr *name) {
   return 0;
 }
 
-ssize_t recv(ShNetworkSocket &s, void *buf, size_t len, int flags) {
+ssize_t recv(ShNetworkSocket& s, void* buf, size_t len, int flags) {
   throw std::logic_error("Not implemented!");
 }
 
-ssize_t recvfrom(ShNetworkSocket &socket, void *buf, size_t len, int flags,
-                 netaddr *from) {
-  rt::UdpConn *sock = socket.data;
+ssize_t recvfrom(ShNetworkSocket& socket, void* buf, size_t len, int flags,
+                 netaddr* from) {
+  rt::UdpConn* sock = socket.data;
   sock->SetNonblocking(socket.nonBlocking);
   return sock->ReadFrom(buf, len, from);
 }
 
-ssize_t recvmsg(ShNetworkSocket &socket, msghdr *message, int flags) {
-  (void)flags;
-  rt::UdpConn *sock = socket.data;
+ssize_t recvmsg(ShNetworkSocket& socket, msghdr* message, int flags) {
+  (void) flags;
+  rt::UdpConn* sock = socket.data;
   sock->SetNonblocking(socket.nonBlocking);
   ssize_t bytesReceived = 0;
   for (size_t i = 0; i < message->msg_iovlen; i++) {
     ssize_t r;
     if (message->msg_name != nullptr) {
-      r = sock->ReadFrom((void *)message->msg_iov[i].iov_base,
-                         (size_t)message->msg_iov[i].iov_len,
-                         (netaddr *)message->msg_name);
+      r = sock->ReadFrom((void*) message->msg_iov[i].iov_base,
+                         (size_t) message->msg_iov[i].iov_len,
+                         (netaddr*) message->msg_name);
     } else {
-      r = sock->Read((void *)message->msg_iov[i].iov_base,
-                     (size_t)message->msg_iov[i].iov_len);
+      r = sock->Read((void*) message->msg_iov[i].iov_base,
+                     (size_t) message->msg_iov[i].iov_len);
     }
     if (r == -1) {
       // Some error happened
@@ -66,11 +66,12 @@ ssize_t recvmsg(ShNetworkSocket &socket, msghdr *message, int flags) {
     }
     bytesReceived += r;
   }
+  // TODO: Create a sockaddr and set it to message->msg_name.
   return bytesReceived;
 }
 
-int recvmmsg(ShNetworkSocket &s, mmsghdr *msgvec, unsigned int vlen,
-             unsigned int flags, timespec *timeout) {
+int recvmmsg(ShNetworkSocket& s, mmsghdr* msgvec, unsigned int vlen,
+             unsigned int flags, timespec* timeout) {
   // Implement via recvmsg
   for (unsigned int i = 0; i < vlen; i++) {
     ssize_t ret = recvmsg(s, &msgvec[i].msg_hdr, flags);
@@ -89,24 +90,24 @@ int recvmmsg(ShNetworkSocket &s, mmsghdr *msgvec, unsigned int vlen,
   return static_cast<int>(vlen);
 }
 
-ssize_t send(ShNetworkSocket &s, const void *buf, size_t len, int flags) {
+ssize_t send(ShNetworkSocket& s, const void* buf, size_t len, int flags) {
   throw std::logic_error("Not implemented!");
 }
 
-ssize_t sendmsg(ShNetworkSocket &socket, const msghdr *message, int flags) {
+ssize_t sendmsg(ShNetworkSocket& socket, const msghdr* message, int flags) {
   VLOG(4) << "Sending message!";
-  rt::UdpConn *sock = socket.data;
+  rt::UdpConn* sock = socket.data;
   sock->SetNonblocking(socket.nonBlocking);
   ssize_t bytesSent = 0;
   for (size_t i = 0; i < message->msg_iovlen; i++) {
     ssize_t r;
     if (message->msg_name != nullptr) {
-      r = sock->WriteTo((void *)message->msg_iov[i].iov_base,
-                        (size_t)message->msg_iov[i].iov_len,
-                        (netaddr *)message->msg_name);
+      r = sock->WriteTo((void*) message->msg_iov[i].iov_base,
+                        (size_t) message->msg_iov[i].iov_len,
+                        (netaddr*) message->msg_name);
     } else {
-      r = sock->Write((void *)message->msg_iov[i].iov_base,
-                      (size_t)message->msg_iov[i].iov_len);
+      r = sock->Write((void*) message->msg_iov[i].iov_base,
+                      (size_t) message->msg_iov[i].iov_len);
     }
     if (r == -1 || size_t(r) != message->msg_iov[i].iov_len) {
       // Some error happened.
@@ -115,31 +116,32 @@ ssize_t sendmsg(ShNetworkSocket &socket, const msghdr *message, int flags) {
     }
     bytesSent += r;
   }
-  VLOG(4) << bytesSent << " bytes sent!";
+  VLOG(4) << bytesSent << " bytes sent to "
+          << rt::NetaddrToIPString(*((netaddr*) message->msg_name));
   return bytesSent;
 }
 
-int sendmmsg(ShNetworkSocket &socket, mmsghdr *msgvec, unsigned int vlen,
+int sendmmsg(ShNetworkSocket& socket, mmsghdr* msgvec, unsigned int vlen,
              int flags) {
   throw std::logic_error("Not implemented!");
 }
 
-ssize_t sendto(ShNetworkSocket &s, const void *buf, size_t len, int flags,
-               const sockaddr *to, socklen_t tolen) {
+ssize_t sendto(ShNetworkSocket& s, const void* buf, size_t len, int flags,
+               const sockaddr* to, socklen_t tolen) {
   throw std::logic_error("Not implemented!");
 }
 
-int shutdown(ShNetworkSocket &s, int how) {
+int shutdown(ShNetworkSocket& s, int how) {
   throw std::logic_error("Not implemented!");
 }
 
 ShNetworkSocket socket() {
   netaddr localAddr{0, 0};
-  rt::UdpConn *sock = rt::UdpConn::Listen(localAddr);
+  rt::UdpConn* sock = rt::UdpConn::Listen(localAddr);
   return ShNetworkSocket(sock);
 }
 
-int set_socket_non_blocking(ShNetworkSocket &s) {
+int set_socket_non_blocking(ShNetworkSocket& s) {
   s.nonBlocking = true;
   return 0;
 }
